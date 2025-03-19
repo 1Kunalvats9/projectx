5ad1315e51b636762a448ad657fa0f4f70e6ff64
Kunal Vats <1kunalvats9@gmail.com>
2025-03-19 20:23:39 +0530

    first commit

---
+++ src/app/about/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      Ye hai about.jsx
+    </div>
+  )
+}
+
+export default page
---
+++ src/app/contact/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      Ye hai contact
+    </div>
+  )
+}
+
+export default page
