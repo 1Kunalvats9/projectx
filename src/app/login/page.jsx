a53b64b88db4bcb3c846f3af3f0e620fe9aff5b8
Kunal Vats <1kunalvats9@gmail.com>
2025-04-05 20:39:49 +0530

    done

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -6,7 +6,6 @@ import React from 'react'
 const page = () => {
   return (
     <div className='w-full h-[100vh] flex items-center flex-col justify-center'>
-      <Navbar />
       <Login />
     </div>
   )
